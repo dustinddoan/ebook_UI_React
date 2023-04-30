a849556b74884da17b40a684438cfa13d3c47a2b
dustinddoan <dustin.ddoan@gmail.com>
2023-04-30 14:04:29 -0700

    add Travis

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe("App component", () => {
+    it("should render app with error", () => {
+        const { asFragment } = render(<App/>);
+        expect(asFragment()).toMatchSnapshot();
+    });
 });
